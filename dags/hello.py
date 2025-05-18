395bfec91c875da9eb9d80cfd9616e7c34738f00
Dominik <dominikzygarski@MacBook-Pro-Dominik.local>
2025-05-18 22:55:53 +0200

    feat: added fetch and preview DAG

--- dags/hello.py
+++ dags/hello.py
@@ -3,27 +3,27 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'datamasterylab.com',
+    'owner': 'dominikzygarski.com',
     'start_date': datetime(2025, 5, 18),
     'catchup': False
 }
 
 dag = DAG(
     'hello_world',
-    default_args = default_args,
-    schedule = timedelta(days=1)
+    default_args=default_args,
+    schedule=timedelta(days=1)
 )
 
 t1 = BashOperator(
-    task_id = 'hello_world',
-    bash_command = 'echo "Hello World"',
-    dag = dag
+    task_id='hello_world',
+    bash_command='echo "Hello World"',
+    dag=dag
 )
 
 t2 = BashOperator(
-    task_id = 'hello_dml',
-    bash_command = 'echo "Hello Data Mastery Lab"',
-    dag = dag
+    task_id='hello_dml',
+    bash_command='echo "Hello Data Mastery Lab"',
+    dag=dag
 )
 
 t1 >> t2 
