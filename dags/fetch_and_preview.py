55fbd7dc738c27dc0e6a9f3b65bd2915e211dc37
Dominik <dominikzygarski@MacBook-Pro-Dominik.local>
2025-05-18 23:50:42 +0200

    updating fetch_and_preview.py file

--- dags/fetch_and_preview.py
+++ dags/fetch_and_preview.py
@@ -1,10 +1,13 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-import requests
-import pandas as pd
 from datetime import datetime, timedelta
 
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
 def get_data(**kwargs):
+    import requests
+    import pandas as pd
+
     url = 'https://raw.githubusercontent.com/airscholar/ApacheFlink-SalesAnalytics/main/output/new-output.csv'
     response = requests.get(url)
 
@@ -16,30 +19,34 @@ def get_data(**kwargs):
 
         kwargs['ti'].xcom_push(key='data', value=json_data)
     else:
-        raise Exception(f'Failded to get data, HTTP status code: {response.status_code}')
-    
+        raise Exception(f'Failed to get data, HTTP status code: {response.status_code}')
+
+
 def preview_data(**kwargs):
-    output_data = kwargs['ti'].xcom_pull(task_ids='get_data', key='data')
+    import pandas as pd
+    import json
+
+    output_data = kwargs['ti'].xcom_pull(key='data', task_ids='get_data')
     print(output_data)
     if output_data:
         output_data = json.loads(output_data)
     else:
         raise ValueError('No data received from XCom')
-    
-    # Create a DataFrame from the JSON data
+
+    # Create Dataframe from JSON data
     df = pd.DataFrame(output_data)
 
-    # Compute total sales
+    #Compute total sales
     df['Total'] = df['Price'] * df['Quantity']
 
     df = df.groupby('Category', as_index=False).agg({'Quantity': 'sum', 'Total': 'sum'})
-    
-    # Sort by total sales
+
+    #sort by total sales
     df = df.sort_values(by='Total', ascending=False)
 
     print(df[['Category', 'Total']].head(20))
 
-    
+
 default_args = {
     'owner': 'dominikzygarski.com',
     'start_date': datetime(2025, 5, 18),
@@ -49,20 +56,19 @@ default_args = {
 dag = DAG(
     'fetch_and_preview',
     default_args = default_args,
-    schedule=timedelta(days=1),
+    schedule=timedelta(days=1)
 )
 
 get_data_from_url = PythonOperator(
     task_id='get_data',
     python_callable=get_data,
-    dag=dag,
+    dag=dag
 )
 
 preview_data_from_url = PythonOperator(
-    task_id='preview_data',
+    task_id ='preview_data',
     python_callable=preview_data,
-    dag=dag,
+    dag=dag
 )
 
 get_data_from_url >> preview_data_from_url
-
